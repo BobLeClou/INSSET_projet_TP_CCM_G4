a0de44fa1f10477aa9181368b6b08bcb1e8985f4
Kerrian Garat <kerriang.22@gmail.com>
2026-01-07 10:40:34 +0100

    feat: put tftates into gcp

--- backend.tf
+++ backend.tf
@@ -1,9 +1,6 @@
-# Configuration du backend pour stocker le tfstate sur GCS
-# À décommenter et configurer après le premier apply
-
-# terraform {
-#   backend "gcs" {
-#     bucket = "g4-insset-projet-2025-tfstate"
-#     prefix = "terraform/state"
-#   }
-# }
+terraform {
+    backend "gcs" {
+        bucket = "g4-insset-projet-2025-tfstate"
+        prefix = "terraform/state"
+    }
+}
