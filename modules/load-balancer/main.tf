35b55dddb4c6941b4872f7ba012ad8c0e73dcdaa
ValentinDuval <vduval457@gmail.com>
2026-01-08 11:20:48 +0100

    Ajout de variables de noms et output pour le load balancer

--- modules/load-balancer/main.tf
+++ modules/load-balancer/main.tf
@@ -1,10 +1,6 @@
 #Subnetwork du proxy loadbalancer. /!\ DOIT ÊTRE SUR LE MÊME RÉSEAU QUE LES VM BACKEND VISÉS.
 resource "google_compute_subnetwork" "proxy_subnet" {
-<<<<<<< HEAD
   name          = var.proxy_subnet_name
-=======
-  name          = "proxy-subnet"
->>>>>>> 8cea8a6 (Definition des variables du load balancer)
   ip_cidr_range = var.proxy_subnet_ip_cidr_range
   network       = var.proxy_subnet_network
   purpose       = "REGIONAL_MANAGED_PROXY"
@@ -43,11 +39,7 @@ resource "google_compute_firewall" "allow_proxy" {
   network       = var.proxy_subnet_network
   priority      = var.firewall_proxy_prority
   source_ranges = [var.proxy_subnet_ip_cidr_range]
-<<<<<<< HEAD
   target_tags   = var.allow_proxy_target_tags
-=======
-  target_tags   = ["load-balanced-backend"]
->>>>>>> 8cea8a6 (Definition des variables du load balancer)
 }
 
 #IP statique réservée au load balancer
@@ -71,7 +63,6 @@ resource "google_compute_region_health_check" "lb_health_check" {
   unhealthy_threshold = 2
 }
 
-<<<<<<< HEAD
 resource "google_compute_region_backend_service" "lb_backend_service" {
   name                  = var.lb_backend_service_name
   load_balancing_scheme = "EXTERNAL_MANAGED"
@@ -92,14 +83,6 @@ resource "google_compute_region_url_map" "lb_url_map" {
   default_service = google_compute_region_backend_service.lb_backend_service.id
 }
 
-=======
-#URL Map pour redistribuer la requête vers la bonne VM
-resource "google_compute_region_url_map" "lb_url_map" {
-  name            = "regional-l7-xlb-map"
-  default_service = google_compute_region_backend_service.lb_health_check.id
-}
-
->>>>>>> 8cea8a6 (Definition des variables du load balancer)
 #Proxy HTTP
 resource "google_compute_region_target_http_proxy" "lb_http_proxy" {
   name    = var.lb_http_proxy_name
@@ -108,11 +91,7 @@ resource "google_compute_region_target_http_proxy" "lb_http_proxy" {
 
 #Règle de renvoi de la requête
 resource "google_compute_forwarding_rule" "lb_forwarding_rule" {
-<<<<<<< HEAD
   name       = var.lb_forwarding_rule_name
-=======
-  name       = "l7-xlb-forwarding-rule"
->>>>>>> 8cea8a6 (Definition des variables du load balancer)
   depends_on = [google_compute_subnetwork.proxy_subnet]
 
   ip_protocol           = "TCP"
